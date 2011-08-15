a08b52d5ffa3cbbea11aceed21ca7e5d54a0476e
Tom Howe <turtlebender@gmail.com>
2011-08-15 14:26:24 -0500

    adding dateutils

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open('README.rst').read()
 CHANGES = open('CHANGES.txt').read()
 
-requires = ['pycassa', 'thrift05', 'setuptools','ordereddict','rdflib','rdfextras']
+requires = ['pycassa', 'thrift05', 'setuptools','ordereddict','rdflib','rdfextras','python-dateutil']
 tests_requires = requires + ['nose', 'mock']
 
 setup(name='agamemnon',
